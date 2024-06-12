acc13c5eaadc03f037f4222e0ae4ba42f23bf44e
dderoover <dennis.deroover@digitalum.eu>
2024-06-12 09:56:35 +0200

    layout changes and small improvements

--- src/app/rsvp/rsvp.component.ts
+++ src/app/rsvp/rsvp.component.ts
@@ -1,9 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'aldero-rsvp',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterModule,
+  ],
   templateUrl: './rsvp.component.html',
   styleUrl: './rsvp.component.scss'
 })
