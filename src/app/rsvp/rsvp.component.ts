b8312d72d628ba433a06ec41feaa5bbb1aac51e9
dennisderoover <dennis.de.roover1@gmail.com>
2024-06-13 00:23:50 +0200

    start page for rsvp and setup for google api

--- src/app/rsvp/rsvp.component.ts
+++ src/app/rsvp/rsvp.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -8,10 +9,32 @@ import { RouterModule } from '@angular/router';
   imports: [
     CommonModule,
     RouterModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './rsvp.component.html',
   styleUrl: './rsvp.component.scss'
 })
 export class RSVPComponent {
+  form = new FormGroup({
+    firstName: new FormControl('', {
+      validators: Validators.required
+    }),
+    lastName: new FormControl('', {
+      validators: Validators.required
+    })
+  })
 
+  public requestInvitation(): void {
+    const credentials = {
+      firstName: this.form.controls.firstName.value,
+      lastName: this.form.controls.lastName.value
+    }
+
+    alert(
+      'searching invitation for: '
+      + credentials.firstName
+      + ' '
+      + credentials.lastName
+    );
+  }
 }
