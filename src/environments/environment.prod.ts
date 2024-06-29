e2056d04235532e3f042ea796031baf8f3501402
dennisderoover <dennis.de.roover1@gmail.com>
2024-06-29 23:44:13 +0200

    test no. 3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.alderotrouwt.be',
+  apiUrl: 'https://alderotrouwt-be.onrender.com',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://api.alderotrouwt.be',
+  apiUrl: 'https://alderotrouwt-be.onrender.com',
 };
 
 /*
