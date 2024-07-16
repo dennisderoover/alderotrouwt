a283155f8d8258aa3c0fe4a7e7aa981c58baf28c
dennisderoover <dennis.de.roover1@gmail.com>
2024-07-16 20:47:22 +0200

    dresscode page

--- src/app/dresscode/dresscode.component.ts
+++ src/app/dresscode/dresscode.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import { PageTitleComponent } from '../shared/page-title/page-title.component';
+import { SubtitleComponent } from '../shared';
 
 @Component({
   selector: 'aldero-dresscode',
   standalone: true,
   imports: [
-    PageTitleComponent
+    PageTitleComponent,
+    SubtitleComponent,
   ],
   templateUrl: './dresscode.component.html',
   styleUrl: './dresscode.component.scss'
 })
 export class DresscodeComponent {
-
+  public readonly title = 'Dresscode inspiratie';
+  public readonly subtitle = `
+    Het thema van onze trouw zal ‘aardse kleuren’ zijn.
+    Hieronder enkele foto’s en een kleurpalette ter inspiratie voor je feestoutfit. 
+    Zeker niet verplicht, maar altijd fijn als je mee in het plaatje past!
+  `
 }
